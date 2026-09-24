f001e4e989777320ea77d8186a9acfa83efe2435
miguelpdavid-ux <miguelpdavid@gmail.com>
2026-09-24 20:18:04 +0100

    Update FM26_AI_MANAGER.cpp

--- FM26_AI_MANAGER.cpp
+++ FM26_AI_MANAGER.cpp
@@ -1,5 +1,15 @@
+#ifndef UNICODE
 #define UNICODE
+#endif
+
+#ifndef _UNICODE
 #define _UNICODE
+#endif
+
+#ifndef NOMINMAX
+#define NOMINMAX
+#endif
+
 #include <windows.h>
 #include <string>
 #include <fstream>
